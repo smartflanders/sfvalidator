21e89d556146a03b9b951c814b9018b5d7513c80
Sander Vanhove <sander.vanhove@ugent.be>
2018-02-26 16:55:48 +0100

    Removed unnecessarily urls.

--- bin/validator.js
+++ bin/validator.js
@@ -3,22 +3,16 @@ var config = {
 		"Cache-Control"
 	],
 	licenses: [
-		"https://purl.org/dc/terms/license",
-		"https://creativecommons.org/ns#license",
 		"http://purl.org/dc/terms/license",
 		"http://creativecommons.org/ns#license"
 	],
 	hydra_links: [
-		"https://www.w3.org/ns/hydra/core#previous", 
-		"https://www.w3.org/ns/hydra/core#next", 
-		"https://www.w3.org/ns/hydra/core#search", 
 		"http://www.w3.org/ns/hydra/core#previous", 
 		"http://www.w3.org/ns/hydra/core#next", 
 		"http://www.w3.org/ns/hydra/core#search"
 	],
 	timestamps: [
-		"http://www.w3.org/ns/prov#generatedAtTime",
-		"https://www.w3.org/ns/prov#generatedAtTime"
+		"http://www.w3.org/ns/prov#generatedAtTime"
 	]
 }
 
