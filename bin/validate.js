aa9e2e74e619d7be0fe16bcd8f6397290dea6f2a
Pieter Colpaert <pieter.colpaert@ugent.be>
2018-03-19 16:20:15 +0100

    More formal indenting

--- bin/validate.js
+++ bin/validate.js
@@ -11,26 +11,25 @@ program
   .arguments('<url-to-validate>')
   .option('-f, --file', 'specify a file of urls')
   .action(input => {
-  	if(program.file){
+    if(program.file){
 
-		var lineReader = require('readline').createInterface({
-			input: require('fs').createReadStream(input)
-		});
+      var lineReader = require('readline').createInterface({
+	input: require('fs').createReadStream(input)
+      });
 
-		lineReader.on('line', line => {
-			var site_args = line.split(" ");
-			if(site_args.length > 1)
-				validator.validate_url(site_args[0], site_args[1]);
-			else
-				validator.validate_url(site_args[0]);
-		});
+      lineReader.on('line', line => {
+	var site_args = line.split(" ");
+	if(site_args.length > 1)
+	  validator.validate_url(site_args[0], site_args[1]);
+	else
+	  validator.validate_url(site_args[0]);
+      });
 
-	}else if(input){
-		validator.validate_url(input);
-	}
+    }else if(input){
+      validator.validate_url(input);
+    }
   })
   .parse(process.argv);
 
 if(!input && !program.file)
-	console.error("Please provide a URL or file path with the option --file.");
-	
+  console.error("Please provide a URL or file path with the option --file.");
